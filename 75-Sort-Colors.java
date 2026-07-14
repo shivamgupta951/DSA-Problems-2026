47881894fb299d0b543759d547faa8c9f05f1838
Shivam Gupta <kapilmohangupta20@gmail.com>
2026-07-14 19:48:00 +0530

    [75] [Time Beats: 100.00%] [Memory Beats: 10.79%] - LeetPush

---
+++ 75-Sort-Colors.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int mid = 0;
+        int low = 0;
+        int high = nums.length-1;
+        while(mid<=high)
+        {
+            if(nums[mid]==2)
+            {
+                nums[mid] = nums[high];
+                nums[high] = 2;
+                high--;
+            }
+            else if(nums[mid]==1)
+            {
+                mid++;
+            }
+            else {
+                nums[mid] = nums[low];
+                nums[low] = 0;
+                low++;
+                mid++;
+            }
+        }
+    }
+}
