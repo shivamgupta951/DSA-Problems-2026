260afcdd42a562f5f06ede3542ececb43a04a881
Shivam Gupta <kapilmohangupta20@gmail.com>
2026-09-14 11:19:00 +0530

    [283] [Time Beats: 91.70%] [Memory Beats: 95.92%] - LeetPush

---
+++ 283-Move-Zeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int idx = 0;
+        int curr = 0;
+        while (curr < nums.length) {
+            if (nums[curr] != 0) {
+                nums[idx] = nums[curr];
+                idx++;
+            }
+            curr++;
+        }
+        while (idx < nums.length) {
+            nums[idx] = 0;
+            idx++;
+        }
+    }
+}
